25ad18bf239c407e2724add076c2d6eb23d915ea
DanielPErez00 <al02850385@tecmilenio.mx>
2019-09-03 02:26:50 -0500

    Edicion del Archivo Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,39 +1,64 @@
- class Calculator {
+
+ 
+ public class Calculator {
 
      private float sum(float x, float y) {
-         float result = 0.0;
-         //ToDo
+         float result = 0.0f;
+         
+         result = x + y;
+         return result;
+
      }
 
      private float mult(float x, float y) {
-         float result = 0.0;
-         //ToDo
+         float result = 0.0f;
+        
+         result = x * y;
+         return result;
+
      }
 
      private float subs (float x, float y) {
-         float result = 0.0;
-         //ToDo
+         float result = 0.0f;
+         
+         result = x - y;
+         return result;
      }
 
      private float mod(float x, float y) {
-         float result = 0.0;
-         //ToDo
+         float result = 0.0f;
+        
+         result = x % y;
+         return result;
      }
 
      private float div(float x, float y) {
-         float result = 0.0;
-         //ToDo
+         float result = 0.0f;
+         result = x / y;
+         return result;
      }
+     
+     
      public void name(String[] args) {
+        
+        String a = args[0];
+        String b = args[1];
 
-        float a = 0.0;
-        float b = 0.0;
+        float x = Float.parseFloat(a);
+        float y = Float.parseFloat(b);
 
-         System.out.printIn("Numero 1: " + x);
-         System.out.printIn("Numero 2 : " + y);
-
-
-
+         System.out.println("Numero 1: " + x);
+         System.out.println("Numero 2 : " + y);
+         System.out.println("Suma = " + this.sum(x, y));
+         System.out.println("Multiplicacion = " + this.mult(x, y));
+         System.out.println("Resta = " + this.subs(x, y));
+         System.out.println("Division = " + this.div(x, y));
+         System.out.println("Modulo = " + this.mod(x, y));
      }
+    
+     public static void main(String[] args) {
 
+     
+         
+     }
 }
