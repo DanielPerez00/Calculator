f59ebfc5a6ab45b4f36e809f31dcbce5e3be3d3f
DanielPerez00 <al02850385@tecmilenio.mx>
2019-09-02 20:47:35 -0500

    Agregar git ignore y calculator.java

---
+++ Calculator.java
@@ -0,0 +1,39 @@
+ class Calculator {
+
+     private float sum(float x, float y) {
+         float result = 0.0;
+         //ToDo
+     }
+
+     private float mult(float x, float y) {
+         float result = 0.0;
+         //ToDo
+     }
+
+     private float subsfloat x, float y() {
+         float result = 0.0;
+         //ToDo
+     }
+
+     private float mod(float x, float y) {
+         float result = 0.0;
+         //ToDo
+     }
+
+     private float div(float x, float y) {
+         float result = 0.0;
+         //ToDo
+     }
+     public void name(String[] args) {
+
+        float a = 0.0;
+        float b = 0.0;
+
+         System.out.printIn("Numero 1: " + x);
+         System.out.printIn("Numero 2 : " + y);
+
+
+
+     }
+
+}
